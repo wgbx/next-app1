d9c8a10bbd7d13dd36dd56d3f1ca8a3be0772087
zhiyongsong-ext <zhiyong.song.ext@1m.app>
2025-01-24 09:57:38 +0800

    add code

--- middleware.js
+++ middleware.js
@@ -1,30 +1,7 @@
 import { NextResponse } from "next/server";
-import cacheHandler from "./cache-handler";
 
 // This function can be marked `async` if using `await` inside
 export async function middleware(request) {
-  const { nextUrl } = request;
-  const { pathname } = nextUrl;
-  console.log("🚀 ~ middleware ~ pathname:", pathname)
-  const cache = new cacheHandler();
-
-  const cacheSubdomain = await cache.get("subdomain");
-
-  if (!cacheSubdomain) {
-    const cache = new cacheHandler();
-    const data = await fetch(
-      "https://release.katana-api.1m.app/subdomain/user",
-      {
-        headers: {
-          from: "szy-server",
-        },
-      }
-    );
-    const dataJson = await data.json();
-    await cache.set("subdomain", JSON.stringify(dataJson.data.items), {
-      tags: ["tag1", "tag2"],
-    });
-  }
 
   return NextResponse.next();
 }
