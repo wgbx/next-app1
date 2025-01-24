64114ba30db14ff5063b22217710257759049e21
zhiyongsong-ext <zhiyong.song.ext@1m.app>
2025-01-24 10:38:18 +0800

    add code

--- app/api/subdomain/route.ts
+++ app/api/subdomain/route.ts
@@ -5,43 +5,15 @@ import fs from "node:fs";
 import os from "node:os";
 import path from "node:path";
 
-const JSON_FILE_NAME = "subdomain.json";
-
-const tempDir = os.tmpdir();
-console.log("🚀 ~ tempDir:", tempDir);
-
 export const GET = async () => {
-  // const jsonData = await getSubdomainJson();
-  // console.log("🚀 ~ GET ~ jsonData:", jsonData);
-  // if (jsonData) {
-  //   return NextResponse.json({ code: 200, data: jsonData });
-  // }
   return NextResponse.json({ code: 200 });
 };
 
 export const POST = async (req) => {
   try {
     const data = await req.json();
-    setSubDomainJson(data);
     return NextResponse.json({ code: 200 });
   } catch (error) {
     return NextResponse.json({ code: 200, error });
   }
 };
-
-function setSubDomainJson(data) {
-  console.log("🚀 ~ setSubDomainJson ~ setSubDomainJson:")
-  const filePath = path.join(tempDir, JSON_FILE_NAME);
-  const dataDir = path.dirname(filePath);
-  if (!fs.existsSync(dataDir)) {
-    fs.mkdirSync(dataDir, { recursive: true });
-  }
-  fs.writeFileSync(filePath, JSON.stringify(data, null, 2));
-  console.log("🚀 ~ setSubDomainJson ~ setSubDomainJson: writeFileSync")
-}
-
-async function getSubdomainJson() {
-  const tempFilePath = path.join(tempDir, JSON_FILE_NAME);
-  const fileContent = fs.readFileSync(tempFilePath, "utf-8");
-  return fileContent;
-}
